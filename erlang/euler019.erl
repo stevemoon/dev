b81b062251610c0cdfe3f9c123f243b050630192
stevemoon <stevemoon@users.noreply.github.com>
2014-06-12 16:32:43 -0400

    updated date function

--- erlang/euler019.erl
+++ erlang/euler019.erl
@@ -2,6 +2,8 @@
 -export([go/0]).
 
 go() ->
-    Months = lists:seq(1,12),
-    Years = lists:seq(1901, 2000),
-    calendar:day_of_the_week(2014, 1, 1).
+    Dates = [{Y, M, D} || Y <- lists:seq(1901, 2000), 
+			  M <- lists:seq(1,12), 
+			  D <- [1]],
+    calendar:day_of_the_week(hd(Dates)).
+
