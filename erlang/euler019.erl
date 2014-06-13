e9a80e1c5e6891fa4be9ae56367d023ff92d6d9f
Steve Moon <steve.moon@gmail.com>
2014-06-12 20:59:13 -0400

    Solved euler019

--- erlang/euler019.erl
+++ erlang/euler019.erl
@@ -5,5 +5,7 @@ go() ->
     Dates = [{Y, M, D} || Y <- lists:seq(1901, 2000), 
 			  M <- lists:seq(1,12), 
 			  D <- [1]],
-    calendar:day_of_the_week(hd(Dates)).
+    WeekDays = [calendar:day_of_the_week(X) || X <- Dates],
+    Sundays = [X || X <- WeekDays, X =:= 7],
+    lists:foldl(fun(X, Sum) -> X + Sum end, 0, Sundays) / 7.
 
