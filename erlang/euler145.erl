5fab0232b9ddf25abe86c46c8fd1702a1b7f2162
stevemoon <stevemoon@users.noreply.github.com>
2014-10-17 09:47:46 -0400

    :)

---
+++ erlang/euler145.erl
@@ -0,0 +1,16 @@
+-module(euler145).
+-export(go/1).
+
+go(Max) ->
+    Sequence = lists:seq(1,Max),
+    go2(Sequence, []).
+
+go2([Next | Sequence], Accum) ->
+    case test(Next) of
+	true ->
+	    go2(Sequence, Next ++ [Accum]);
+	false ->
+	    go2(Sequence, Accum).
+
+test(Num) ->
+    
