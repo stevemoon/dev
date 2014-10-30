4cb36ea4400b775b3c6da13cf1ffb85222895fc0
stevemoon <stevemoon@users.noreply.github.com>
2014-10-30 16:12:20 -0400

    Messing around with how to do moves

--- erlang/iq.erl
+++ erlang/iq.erl
@@ -1,5 +1,5 @@
 -module(iq).
--export([go/1]).
+-export([go/1, find_moves/2, solve/2, move/4]).
 -compile([debug_info]).
 %
 %     A
@@ -18,39 +18,142 @@ solve(Board, Moves) ->
     Peg_Count = count_pegs(Board),
     case Peg_Count of 
         1 -> io:format("Solution found: ~s~n", [Moves]);
-	_ -> find_moves(Board, Moves)
+	_ -> try spawn(iq, find_moves, [Board, Moves])
+	     catch error -> ok end
     end.
 
-find_moves([A, B, C, D, E, F, G, H, I, J, K, L, M, N, O], Moves) ->
-    Initial_Board = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O],
+move(Board, From, Takes, To) ->
+    %% [{"A", A}, {"B", B}, {"C", C}, {"D", D},{"E", E}, {"F", F}, {"G", G}, 
+    %%  {"H", H}, {"I", I}, {"J", J}, {"K", K}, {"L", L}, {"M", M}, {"N", N}, 
+    %%  {"O", O}] 
+    [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O] = Board,
+    pretty_print(Board),
+    X = hd(From) - 64,
+    Y = hd(Takes) - 64,
+    Z = hd(To) - 64,
+    io:format("~w~w~w~n", [X,Y,Z]).
+    
+
+%%  when From < Takes && Takes < To ->
+%%     true;
+%% move(Board, From, Takes, To) when From < Takes && Takes > To ->
+%%     false;
+%% move(Board, From, Takes, To) when From > Takes && Takes < To ->
+%%     goober;
+%% move(Board, From, Takes, To) when From > Takes && Takes > To ->
+
+
+
+find_moves([1, B, C, D, E, F, G, H, I, J, K, L, M, N, O], Moves) ->
+    Initial_Board = [1, B, C, D, E, F, G, H, I, J, K, L, M, N, O],
     % AxB->D, AxC->F
     case Initial_Board of
-        [1, 1, _, 0, _, _, _, _, _, _, _, _, _, _, _] -> spawn(solve([0, 0, C, 1, E, F, G, H, I, J, K, L, M, N, O], Moves ++ "AxB->D  "));
+        [1, 1, _, 0, _, _, _, _, _, _, _, _, _, _, _] -> 
+	    try spawn(iq, solve, [[0, 0, C, 1, E, F, G, H, I, J, K, L, M, N, O], Moves ++ "AxB->D  "])
+	    catch error -> ok end;	    
 	_ -> false
     end,
+    io:format("~s~n", ["1st case of A"]),
     case Initial_Board of
-        [1, _, 1, _, _, 0, _, _, _, _, _, _, _, _, _] -> spawn(solve([0, B, 0, D, E, 1, G, H, I, J, K, L, M, N, O], Moves ++ "AxC->F  "));
+        [1, _, 1, _, _, 0, _, _, _, _, _, _, _, _, _] -> 
+	    try spawn(iq, solve, [[0, B, 0, D, E, 1, G, H, I, J, K, L, M, N, O], Moves ++ "AxC->F  "])
+	    catch error -> ok end;
 	_ -> false
     end,
+    io:format("~s~n", ["2nd case of A"]);
+find_moves([A, 1, C, D, E, F, G, H, I, J, K, L, M, N, O], Moves) ->
+    Initial_Board = [A, 1, C, D, E, F, G, H, I, J, K, L, M, N, O],
     % BxD->G, BxE->I
+    case Initial_Board of
+	[_, 1, _, 1, _, _, 0, _, _, _, _, _, _, _, _] -> 
+	    try spawn(iq, solve, [[A, 0, C, 0, E, F, 1, H, I, J, K, L, M, N, O], Moves ++ "BxD->G  "])
+	    catch error -> ok end;
+	_ -> false
+    end,
+    io:format("~s~n", ["1st case of B"]),
+    case Initial_Board of
+	[_, 1, _, _, 1, _, _, _, 0, _, _, _, _, _, _] -> 
+	    try spawn(iq, solve, [[A, 0, C, D, 0, F, G, H, 1, J, K, L, M, N, O], Moves ++ "BxE->I  "])
+	    catch error -> ok end;
+	_ -> false
+    end,
+    io:format("~s~n", ["2nd case of B"]);
+find_moves([A, B, 1, D, E, F, G, H, I, J, K, L, M, N, O], Moves) ->
+    Initial_Board = [A, B, 1, D, E, F, G, H, I, J, K, L, M, N, O],
     % CxE->H, CxF->J
+    case Initial_Board of
+       %[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]
+	[_, _, 1, _, 1, _, _, 0, _, _, _, _, _, _, _] -> 
+	    %                     [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]
+	    try spawn(iq, solve, [[A, B, 0, D, 0, F, G, 1, I, J, K, L, M, N, O], Moves ++ "CxE->H  "])
+	    catch error -> ok end;
+	_ -> false
+    end,
+    io:format("~s~n", ["1st case of C"]),
+    case Initial_Board of
+       %[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]
+	[_, _, 1, _, _, 1, _, _, _, 0, _, _, _, _, _] -> 
+	    %                     [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]
+	    try spawn(iq, solve, [[A, B, 0, D, E, 0, G, 1, I, 1, K, L, M, N, O], Moves ++ "CxF->J  "])
+	    catch error -> ok end;
+	_ -> false
+    end,
+    io:format("~s~n", ["2nd case of C"]);
+find_moves([A, B, C, 1, E, F, G, H, I, J, K, L, M, N, O], Moves) ->
+    Initial_Board = [A, B, C, 1, E, F, G, H, I, J, K, L, M, N, O],
     % DxB->A, DxE->F, DxH->M, DxG->K
     case Initial_Board of
-	[_, _, _, 1, _, _, 1, _, _, _, 0, _, _, _, _] -> spawn(solve([A, B, C, 0, E, F, 0, H, I, J, 1, L, M, N, O], Moves ++ "DxG->K  "));
-         _ -> false
-    end.
-    % ExH->L, ExI->N
-    % FxC->A, FxE->D, FxI->M, FxJ->O
-    % GxD->B, GxH->I
-    % HxE->C, HxI->J
-    % IxH->G, IxE->B
-    % JxI->H, JxF->C
-    % KxG->D, KxL->M
-    % LxH->E, LxM->N
-    % MxL->K, MxH->D, MxI->F, MxN-O
-    % NxM->L, NxI->E
-    % OxN->M, OxJ->F
-%1.
+       %[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]
+	[0, 1, _, 1, 1, _, _, _, _, _, _, _, _, _, _] -> 
+	    %                     [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]
+	    try spawn(iq, solve, [[1, 0, C, 0, E, F, G, H, I, J, K, L, M, N, O], Moves ++ "DxB->A  "])
+	    catch error -> ok end;
+	_ -> false
+    end,
+    io:format("~s~n", ["1st case of D"]),
+    case Initial_Board of
+       %[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]
+	[_, _, _, 1, 1, 0, _, _, _, _, _, _, _, _, _] -> 
+	    %                     [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]
+	    try spawn(iq, solve, [[A, B, C, 0, 0, 1, G, H, I, J, K, L, M, N, O], Moves ++ "DxE->F  "])
+	    catch error -> ok end;
+	_ -> false
+    end,
+    io:format("~s~n", ["2nd case of D"]),
+    case Initial_Board of
+       %[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]
+	[_, _, _, 1, _, _, _, 1, _, _, _, _, 0, _, _] -> 
+	    %                     [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]
+	    try spawn(iq, solve, [[A, B, C, 0, E, F, G, 0, I, J, K, L, 1, N, O], Moves ++ "DxH->M  "])
+	    catch error -> ok end;
+	_ -> false
+    end,
+    io:format("~s~n", ["3rd case of D"]),
+    case Initial_Board of
+       %[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]
+	[_, _, _, 1, _, _, 1, _, _, _, 0, _, _, _, _] -> 
+	    %                     [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]
+	    try spawn(iq, solve, [[A, B, C, 0, E, F, 0, H, I, J, 1, L, M, N, O], Moves ++ "DxG->K  "])
+	    catch error -> ok end;
+	_ -> false
+    end,
+    io:format("~s~n", ["4th case of D"]).
+%%     case Initial_Board of
+%% 	[_, _, _, 1, _, _, 1, _, _, _, 0, _, _, _, _] -> spawn(solve([A, B, C, 0, E, F, 0, H, I, J, 1, L, M, N, O], Moves ++ "DxG->K  "));
+%%          _ -> false
+%%     end.
+%%     % ExH->L, ExI->N
+%%     % FxC->A, FxE->D, FxI->M, FxJ->O
+%%     % GxD->B, GxH->I
+%%     % HxE->C, HxI->J
+%%     % IxH->G, IxE->B
+%%     % JxI->H, JxF->C
+%%     % KxG->D, KxL->M
+%%     % LxH->E, LxM->N
+%%     % MxL->K, MxH->D, MxI->F, MxN-O
+%%     % NxM->L, NxI->E
+%%     % OxN->M, OxJ->F
+%% %1.
     
 pretty_print([A, B, C, D, E, F, G, H, I, J, K, L, M, N, O]) ->
     io:format("    ~B~n", [A]),
