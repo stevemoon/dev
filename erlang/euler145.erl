eeb4e373bcadbd4bb54fa6a5c79b7d893fb1ce63
Steve Moon <steve.moon@gmail.com>
2015-02-06 22:24:00 -0500

    Solved euler145

--- erlang/euler145.erl
+++ erlang/euler145.erl
@@ -1,9 +1,13 @@
 -module(euler145).
 -export([go/1, test/1]).
 
+% Answer is 608720
+% This takes a long time to run (~1 hour) for 1,000,000,000. There must be some serious
+% optimization I am missing...
+
 go(Max) ->
     Sequence = lists:seq(10,Max),
-    io:format("~w~n", [Sequence]),
+    %io:format("~w~n", [Sequence]),
     go2(Sequence, []).
 
 go2([Next | Sequence], Accum) ->
@@ -19,18 +23,20 @@ go2([],Accum) ->
     length(Accum).
 
 
-test(Num) ->
+test(Num) when (Num rem 10 =/= 0) ->
     Fwd = integer_to_list(Num),
     Bwd = lists:reverse(Fwd),
     BwdNum = list_to_integer(Bwd),
-  
     Sum = Num + BwdNum,
     SumList = [list_to_integer([X]) || X <- integer_to_list(Sum)],
     EvenDigits = [X || X <- SumList, (X rem 2 =:= 0)],
+    %io:format("Sum: ~w  SumList: ~w   EvenDigits: ~w~n", [Sum, SumList, EvenDigits]),    
     case length(EvenDigits) of
 	0 -> true;
 	     %io:format("Num: ~w BwdNum: ~w ~n", [Num, BwdNum]),
-	     %io:format("Sum: ~w  SumList: ~w   EvenDigits: ~w~n", [Sum, SumList, EvenDigits]);
 	_ -> false
-        end.
+    end;
+test(_) ->
+    false.
+
 
