bf5e983368837a0d1e8c9bb86770c56df7ab4d35
Steve Moon <steve.moon@gmail.com>
2015-02-26 22:42:31 -0500

    Solved Euler 059!

--- erlang/euler059.erl
+++ erlang/euler059.erl
@@ -1,29 +1,46 @@
 -module(euler059).
--export([go/0, foobar/1]).
+-export([go/0, foobar/1, sum_plaintext/1]).
 
-%need to fix file read using fread instead of read so I get integers
-%http://stackoverflow.com/questions/10295557/erlang-reading-integers-from-file
 % based on excel analysis 79, 68, and 71 are the 3 most common characters.
 % The most common text character is a space, so the key is likey a combination of d-g-o
 % xor space (32) with each of the common numbers and you get d-g-o.
 
+% 1> c(euler059).
+% {ok,euler059}
+% 2> euler059:go().
+% {match,[{36,5}]} {103,111,100} 
+% (The Gospel of John, chapter 1) 1 In the beginning the Word already existed. He was with God, 
+% Based on brute force the key is "god".
+%
+% 3> euler059:foobar([103,111,100]).
+% "(The Gospel of John, chapter 1) 1 In the beginning the Word al
+% -- Sure enough -- that works!
+%
+% my fread function doesn't catch the last integer correctly. Rather than fix the function I just added
+% a trailing comma so the pattern matches correctly.
+
+% And the answer is (drumroll please...)
+% 5> euler059:sum_plaintext([103,111,100]).
+% 107359
+
 go() ->
-    FileData = readlines("p059_cipher.txt"),
-    CipherText = hd(hd(string:tokens(FileData, ","))),
-    %io:format("~s~n", [CipherText]),
-    %Key = [103,104,105],
+    CipherText = readlines("p059_cipher.txt"),
     brute_force(CipherText, 97, 97, 97).
-    %brute_force(97, 97, 97).
 
 foobar(Key) ->
-    FileData = readlines("p059_cipher.txt"),
-    CipherText = string_to_list(FileData),
-    io:format("~w~n", [CipherText]),
+    CipherText = readlines("p059_cipher.txt"),
     decrypt(CipherText, Key, []).
+
+sum_plaintext(Key) ->
+    CipherText = readlines("p059_cipher.txt"),
+    PlainText = decrypt(CipherText, Key, []),
+    lists:foldl(fun(X, Sum) -> X + Sum end, 0, PlainText).
+
+
 
 brute_force(CipherText, Key1, Key2, Key3) when (Key1 =< 122) and (Key2 =< 122) and (Key3 =< 122) ->
 	Trial = decrypt(CipherText, [Key1, Key2, Key3], []),
-    Result = re:run(Trial, " THE "),
+    Result = re:run(Trial, " the "),
     case Result of
     	nomatch -> 	brute_force(CipherText, Key1, Key2, Key3 + 1);
     	_ -> 		io:format("~w ~w ~n", [Result, {Key1, Key2, Key3}]),
@@ -39,22 +56,23 @@ brute_force(_, Key1, _, _) when Key1 =:= 123 ->
 
 decrypt([CipherHead | CipherTail], [KeyHead | KeyTail], Accum) ->
 	PlainText = CipherHead bxor KeyHead,
-	%PlainText = CipherHead,
-	%io:format("~s xor ~s ~n", [[CipherHead], [KeyHead]]),
 	decrypt(CipherTail, KeyTail ++ [KeyHead], Accum ++ [PlainText]);
 decrypt([],_,Accum) ->
-	%io:format("~s ~n", [Accum]).
 	Accum.
 
-get_all_lines(Device, Accum) ->
-    case io:get_line(Device, "") of
-        eof  -> file:close(Device), Accum;
-        Line ->
-            SLine = string:strip(Line, right, $\n),
-            get_all_lines(Device, [SLine|Accum])
-    end.
+readlines(Filename) ->
+    {ok, IoDev} = file:open(Filename, [read]),
+    read([], IoDev).
 
-readlines(FileName) ->
-    {ok, Device} = file:open(FileName, [read]),
-    lists:reverse(get_all_lines(Device, [])).
+read(List, IoDev) ->
+    case io:fread(IoDev, "", "~d,") of
+        {ok, [A]} ->
+            %io:format("~w ", [A]),
+            read([A | List], IoDev);
+        eof ->
+            lists:reverse(List);
+        {error, _} ->
+            failed,
+            read(List, IoDev)
+    end.
  
