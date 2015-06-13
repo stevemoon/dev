bc9aa43bd6b179f49ba280ebb94d644bfe66ede1
Steve Moon <steve.moon@gmail.com>
2015-06-12 20:10:18 -0400

    junk

--- node/hw.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var server = http.createServer();
-server.on('request', function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World\n');
-    	console.log('Request handled.\n');
-	})
-    server.listen(3000);
-console.log('Server running at http://localhost:3000/');
--- node/image-server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'image/jpg'});
-	fs.createReadStream('./image.jpg').pipe(res);
-}).listen(3000);
-console.log('Server running...');
