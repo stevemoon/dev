52ab4fb30dd2b5fb52c4c4b3447cb989a9e8cd07
Steve Moon <steve.moon@gmail.com>
2015-09-15 10:56:23 -0400

    starting work on euler24

---
+++ erlang/euler024.erl
@@ -0,0 +1,19 @@
+%%% Euler 024
+%%%
+%%% Algorithm from: http://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+
+-module(euler024).
+-export([go/1]).
+
+go(Seed) ->
+    ok.
+
+next_lexicographic(X) ->
+    X.
+
+id_pivot(X) ->
+    X.
+
+id_pivot_successor(Front, Pivot, End) ->
+    ok.
+
