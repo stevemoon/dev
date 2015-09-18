4dbd4372dbcaf5a1891223cf971c896ea9e0fed5
stevemoon <stevemoon@users.noreply.github.com>
2015-09-18 16:20:27 -0400

    Solved Euler024
    It isn't pretty, lazily using lists:flatten excessively, but it works
    and only takes a couple of seconds to run.

--- erlang/euler024.erl
+++ erlang/euler024.erl
@@ -7,17 +7,30 @@
 %%% 012   021   102   120   201   210
 %%%
 %%% What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
-%%%
+%%%  euler024:go([0,1,2,3,4,5,6,7,8,9]).
+%%%  [2,7,8,3,9,1,5,4,6,0]
+%%%  --- Answer is 2783915460
 %%% Algorithm from: http://www.nayuki.io/page/next-lexicographical-permutation-algorithm
 
 -module(euler024).
 -export([go/1]).
 
 go(Seed) ->
+    nth_lex(Seed, 1000000).
+
+nth_lex(Seed, 1) ->
+    Seed;
+nth_lex(Seed, Count) ->
+    %io:format("~p~n", [Seed]),
+    NextSeed = next_lex(Seed),
+    nth_lex(NextSeed, Count - 1).
+
+next_lex(Seed) ->
     RevSeed = lists:reverse(Seed),
-    {ok, Suffix, Pivot, Prefix} = find_non_increasing_prefix(RevSeed),
-    io:format("~p ~p ~p ~n~n", [Suffix, Pivot, Prefix]),
-    find_correct_swap_element(Suffix, Pivot).
+    {ok, Prefix, Pivot, Suffix} = find_non_increasing_prefix(RevSeed),
+    NewSuffix = find_new_suffix(Suffix, Pivot),
+    NextLex = lists:flatten([[Prefix],[NewSuffix]]),
+    NextLex.
 
 find_non_increasing_prefix(Seed) -> % Use reverse first for suffix
     fnip([hd(Seed)], tl(Seed)).
@@ -26,19 +39,23 @@ fnip([A | B], [C | D]) when A =< C ->
     fnip(X, D);
 fnip([A | B], [C | D]) when A > C -> % We've arrived!
     X = lists:flatten([[A], [B]]),
-    {ok, X, C, D };
+    {ok, lists:reverse(D), C, X };
 fnip(A, []) -> % We've hit the end
     {error, no_more_permutations, A}.
 
-find_correct_swap_element(Suffix, Pivot) ->
-    fcse(Pivot, hd(Suffix), tl(Suffix)).
-fcse(Pivot, Tested, [Test | Remaining]) when Pivot >= Test ->
-    io:format("tested: ~p~n", [Tested]),
+find_new_suffix(Suffix, Pivot) ->
+    RevSuffix = lists:reverse(Suffix),
+    fns(Pivot, [], RevSuffix).
+fns(Pivot, Tested, []) ->
+    NewSuffix = lists:flatten([[Tested], [Pivot]]),
+    NewSuffix;
+fns(Pivot, Tested, [Test | Remaining]) when (Pivot >= Test) ->
+    %io:format("tested: ~p~n", [Tested]),
     X = lists:flatten([Tested],[Test]),
-    fcse(Pivot, X, Remaining);
-fcse(Pivot, Tested, [Test | Remaining]) when Pivot < Test ->
+    fns(Pivot, X, Remaining);
+fns(Pivot, Tested, [Test | Remaining]) when Pivot < Test ->
     NewSuffix = lists:flatten([[Test],[Tested], [Pivot], [Remaining]]),
-    io:format("~p ~p ~p ~p ~n", [Pivot, Tested, Test, Remaining]),
+    %io:format("~p ~p ~p ~p ~n", [Pivot, Tested, Test, Remaining]),
     NewSuffix.
 
 
