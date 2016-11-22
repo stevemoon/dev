d49643def1223e21fea354d18f78b7e7abf9dbc7
stevemoon <stevemoon@users.noreply.github.com>
2016-11-22 16:08:11 -0500

    Mostly working. Score is off-by-one, and player1 and player 2 are reversed. Next steps: compare output with working Erlang version and find the offending line in the source file that scores wrong. Also figure out why player1 and player2 scores are swapped.

--- elixir/euler054/lib/euler054.ex
+++ elixir/euler054/lib/euler054.ex
@@ -1,5 +1,5 @@
 defmodule Euler054 do
-  @chunksize 1
+  @chunksize 207
   @cardval %{'1' => 1,  '2' => 2,  '3' => 3,  '4' => 4, '5' => 5,
              '6' => 6,  '7' => 7,  '8' => 8,  '9' => 9, 'T' => 10,
              'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14}
@@ -10,7 +10,17 @@ defmodule Euler054 do
     # |> Enum.each(&IO.inspect(extract_hands(&1) |> IO.inspect))
     # |> Enum.each(&IO.inspect(extract_hands(&1) ))
     #|> Enum.each(&IO.inspect(extract_hands(&1) |> decode_hands([])))
-    |> Enum.each(&IO.inspect(extract_hands(&1) |> process_hands))
+    |> Enum.to_list
+    |> List.flatten
+    |> extract_hands
+    |> process_hands
+
+    # |> Enum.each(&IO.inspect(extract_hands(&1) |> process_hands))
+    # |> Enum.each(&Enum.reduce({0, 0, 0}, fn({p1, p2, tie}, {accp1, accp2, acctie} -> ))
+    # IO.inspect(foo)
+    # |> Enum.into(&1, [])
+  #  (&extract_hands(&1) |> process_hands))
+    #|> Enum.reduce()
     #probably replace Enum.each with Enum.map then pipe the resulting list to a reduce
 
     #Enum.each IO.stream(file, :line), &IO.inspect(extract_hands(&1))
@@ -77,8 +87,6 @@ def process_hand({p1hand, p2hand}) do
   end
 end
 
-def score_hand(_), do: 100
-
 
 def resolve_tie([],[]), do: :tie
 def resolve_tie([p1max | _p1tail], [p2max | _p2tail]) when p1max > p2max, do: :p1
@@ -89,113 +97,107 @@ end
 def resolve_tie(_, _), do: :error
 
 def sort_cards(cards) do
-  IO.inspect(cards)
+  #IO.inspect(cards)
   {val, _} = Enum.unzip(cards)
   Enum.sort(val)
 end
-#
-#score_hand(Cards) ->
-#    SortedCards = sort_cards(Cards), %strips out suit
-#    High_Card = hd(lists:reverse(SortedCards)),
-#    One_pair = find_pair(SortedCards),
-#    Two_pair = find_2pair(SortedCards),
-#    Three_kind = find_3kind(SortedCards),
-#    Straight = find_straight(SortedCards),
-#    Flush = find_flush(Cards),
-#    Full_House = find_full_house(SortedCards),
-#    Four_kind = find_4kind(SortedCards),
-#    Straight_Flush = find_straight_flush(Flush, Straight),
-#    Royal_Flush = find_royal_flush(High_Card, Straight_Flush),
-#    ScoreList = [High_Card, One_pair, Two_pair, Three_kind, Straight, Flush,
-#                 Full_House, Four_kind, Straight_Flush, Royal_Flush],
-#    HighScore = hd(lists:reverse(lists:sort(ScoreList))),
-#    HighScore.
-#
-#find_pair([First, Second, Third, Fourth, Fifth]) ->
-#    %22345, 23345, 23445, 23455
-#    Result = if (First =:= Second) or (Second =:= Third) -> Second * 2 * 10;
-#                (Third =:= Fourth) or (Fourth =:= Fifth) -> Fourth * 2 * 10;
-#                true -> 0
-#            end,
-#    Result.
-#
-#find_2pair([First, Second, Third, Fourth, Fifth]) ->
-#    %22334, 23344,22344
-#    Result = if (First =:= Second) and (Third =:= Fourth) -> (Second + Fourth) * 2 * 100;
-#                (Second =:= Third) and (Fourth =:= Fifth) -> (Second + Fourth) * 2 * 100;
-#                (First =:= Second) and (Fourth =:= Fifth) -> (Second + Fourth) * 2 * 100;
-#                true -> 0
-#            end,
-#    Result.
-#
-#find_3kind([First, Second, Third, Fourth, Fifth]) ->
-#    % 3-kind could be 3 patterns: 22233, 22333, 23334
-#    Result = if (First =:= Second) and (Second =:= Third) -> Second * 3 * 1000;
-#                (Second =:= Third) and (Third =:= Fourth) -> Second * 3 * 1000;
-#                (Third =:= Fourth) and (Fourth =:= Fifth) -> Fourth * 3 * 1000;
-#                true -> 0
-#            end,
-#    Result.
-#
-#find_straight([Current, Next | Rest]) when (Next - Current) =:= 1 ->
-#    find_straight([Next] ++ Rest);
-#find_straight([Current, Next | _]) when (Next - Current) =/= 1 ->
-#    0;
-#find_straight(Last) when length(Last) =:= 1 ->
-#    hd(Last) * 10000.
-#
-#
-#find_flush(Cards) ->
-#    {_, Suit} = hd(Cards),
-#    SameSuit = [X || {X, XSuit} <- Cards, XSuit == Suit],
-#    case length(SameSuit) == 5 of
-#        true ->
-#            hd(lists:reverse(sort_cards(Cards))) * 100000;
-#        false -> 0
-#    end.
-#
-#find_full_house([First, Second, Third, Fourth, Fifth]) ->
-#    %22333, 22233
-#    Result = if (First =:= Second) and (Third =:= Fifth) -> (First * 2 + Third * 3) * 1000000;
-#                (First =:= Third) and (Fourth =:= Fifth) -> (First * 3 + Fourth * 2) * 1000000;
-#                true -> 0
-#            end,
-#    Result.
-#
-#find_4kind([First, Second, _Third, Fourth, Fifth]) ->
-#    Result = if (First =:= Fourth) -> Fourth * 4 * 10000000;
-#                (Second =:= Fifth) -> Fourth * 4 * 10000000;
-#                true -> 0
-#            end,
-#    Result.
-#
-#
-#find_straight_flush(Is_Flush, Is_Straight) ->
-#    Result = if (Is_Flush > 0) and (Is_Straight > 0) -> Is_Straight / 10000 * 1000000000;
-#                true -> 0
-#            end,
-#    Result.
-#
-#find_royal_flush(High_Card, Is_Straight_Flush) when (Is_Straight_Flush  > 0) and (High_Card =:= 14) ->
-#    io:format("~s ~n", ["Royal Flush!"]),
-#    100000000000;
-#find_royal_flush(_, _) ->
-#    0.
-#readlines(FileName) ->
-#    {ok, Device} = file:open(FileName, [read]),
-#    lists:reverse(get_all_lines(Device, [])).
-#
-#get_all_lines(Device, Accum) ->
-#    case io:get_line(Device, "") of
-#        eof  -> file:close(Device), Accum;
-#        Line ->
-#            SLine = string:strip(Line, right, $\n),
-#            {P1Hand, P2Hand} = extract_hands(SLine),
-#            P1_decoded_hand = decode_cards(P1Hand, []),
-#            P2_decoded_hand = decode_cards(P2Hand, []),
-#            get_all_lines(Device, [{P1_decoded_hand, P2_decoded_hand}|Accum])
-#    end.
-#
+
+def score_hand(cards) do
+  sc = sort_cards(cards)
+  scorelist = [
+    Enum.max(sc), #High card
+    one_pair?(sc),
+    two_pair?(sc),
+    three_kind?(sc),
+    straight?(sc),
+    flush?(cards, sc),
+    full_house?(sc),
+    four_kind?(sc),
+    straight_flush?(cards, sc),
+    royal_flush?(cards,sc)
+  ]
+  #IO.inspect(scorelist)
+  Enum.max(scorelist)
+end
+
+def one_pair?([a, b, c, d, e]) do
+  #22345, 23345, 23445, 23455
+  cond do
+    d == e -> e * 2 * 10
+    c == d -> d * 2 * 10
+    b == c -> c * 2 * 10
+    a == b -> b * 2 * 10
+    true -> 0
+  end
+end
+
+def two_pair?([a, b, c, d, e]) do
+  #22334, 23344, 22344
+  cond do
+  a == b && c == d -> (b + d) * 2 * 100
+  b == c && d == e -> (c + e) * 2 * 100
+  a == b && d == e -> (b + e) * 2 * 100
+  true -> 0
+  end
+end
+
+def three_kind?([a, b, c, d, e]) do
+  #22234, 23334, 23444
+  cond do
+    a == c -> c * 3 * 1000
+    b == d -> d * 3 * 1000
+    c == e -> e * 3 * 1000
+    true -> 0
+  end
+end
+
+def straight?([current, next | rest]) when (next - current) == 1 do
+  straight?([next] ++ rest)
+end
+def straight?([current, next | _ ]) when (next - current) != 1, do: 0
+def straight?(last) when length(last) == 1, do: hd(last) * 10000
+
+def flush?(cards, sc) do
+  {_, suit} = hd(cards)
+  samesuit = for {_, xsuit} <- cards, suit == xsuit do xsuit end
+  case length(samesuit) do
+    5 -> Enum.max(sc) * 100000
+    _ -> 0
+  end
+end
+
+def full_house?([a, b, c, d, e]) do
+  #22233, 22333
+  cond do
+    a == c && d == e -> (a * 3 + d * 2) * 1000000
+    a == b && c == e -> (a * 2 + c * 3) * 1000000
+    true -> 0
+  end
+end
+
+def four_kind?([a, b, _c, d, e]) do
+  #22223, 23333
+  cond do
+    a == d -> d * 4 * 10000000
+    b == e -> e * 4 * 10000000
+    true -> 0
+  end
+end
+
+def straight_flush?(cards, sc) do
+  cond do
+    straight?(sc) > 0 && flush?(cards, sc) > 0 -> Enum.max(sc) / 10000 * 1000000000
+    true -> 0
+  end
+end
+
+def royal_flush?(cards,sc) do
+  cond do
+    straight?(sc) > 0 && flush?(cards, sc) > 0 && Enum.max(sc) == 14 -> 100000000000
+    true -> 0
+  end
+end
+
 def extract_hands(hands, accum \\ [])
 def extract_hands([], accum) do accum end
 def extract_hands([hand | rest], accum) do
@@ -212,18 +214,3 @@ def decode_cards([card | rest], accum) do
 end
 
 end
-#Given: list of tuples where each tuple contains 2 lists
-# these lists represent a hand for each player
-#Returns: A list representing the same data but decoded from
-# [{["2S", "2C"], ["4C", "5D"]}] into
-#[ { [{2, :spades}, {2, :clubs}], [{4, :clubs}, {5, :diamonds}] } ]
-# def decode_hands([], accum), do: accum
-# def decode_hands([{p1, p2} | rest], accum) do
-  # decoded_p1 = decode_cards(p1, [])
-  # decoded_p2 = decode_cards(p2, [])
-  # decode_hands(rest, [{decoded_p1, decoded_p2} | accum])
-# end
-
-#Given: list of cards in the format "2S" (in this case 2 of spades)
-#Returns: list of tuples, each tuple is a card in the format
-# {2, :spades}
